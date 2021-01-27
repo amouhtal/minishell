3e63e08e830638dbbaa31e7c3e22236d7d9cf700
Abderrahmane Mouhtal <amouhtal@e2r11p1.1337.ma>
2021-01-27 16:46:19 +0100

    amouhtal

--- main.c
+++ main.c
@@ -46,41 +46,40 @@ void get_first(t_env **env)
 	{
 		(*env) = (*env)->prev;
 	}
-	
 }
 void printf_arg_cmd(t_token *first)
 {
 	int i;
 
-	i= 0;
+	i = 0;
 	while (first->prev)
 		first = first->prev;
 	while (first->next)
 	{
-		printf("type=%d\n",first->type);
-		printf("str=%s\n",first->str);
+		printf("type=%d\n", first->type);
+		printf("str=%s\n", first->str);
 		printf("=============================\n");
-		first=first->next;
+		first = first->next;
 	}
-	printf("type=%d\n",first->type);
-	printf("str=%s\n",first->str);
+	printf("type=%d\n", first->type);
+	printf("str=%s\n", first->str);
 	printf("=============================\n");
 	i = 0;
 }
 void test(t_token *token)
 {
-		while (token->prev)
+	while (token->prev)
 		token = token->prev;
 }
 int main(int argc, char **argv, char **env)
 {
 	t_minishell minishell;
- 	char *line;
+	char *line;
 	int s;
 	int status;
 	// t_env *temp;
 	s = 1;
-	minishell.env=get_env(env,argv,argc);
+	minishell.env = get_env(env, argv, argc);
 	increment_shlvl(minishell.env);
 	status = 1;
 	signal(SIGINT, ft_cntr_c);
@@ -89,30 +88,30 @@ int main(int argc, char **argv, char **env)
 	// temp = mini.env;
 	// while (temp->prev)
 	// 	temp = temp->prev;
-		
+
 	// while (temp)
 	// {
 	// 	printf("%s\n",temp->value);
 	// 	temp= temp->next;
 	// }
-	
-	while(status)
+
+	while (status)
 	{
 		line = NULL;
 		minishell.token = new_token();
 		ft_putstr_fd("\033[1;34m$minishell$~> 👾\033[0m", 1);
 		int r = get_next_line(0, &line);
 		if (r == 0)
-			{
-				ft_putstr_fd("exit\n", 1);
-				exit(0);
-			}
-		if(line[0] != '\0')
+		{
+			ft_putstr_fd("exit\n", 1);
+			exit(0);
+		}
+		if (line[0] != '\0')
 		{
 			minishell.stdi = dup(0);
 			minishell.stdo = dup(1);
 			//puts("here");
-			check_fir(&minishell,line);
+			check_fir(&minishell, line);
 			check_sec(&minishell);
 			//printf_arg_cmd(minishell.token);
 			// printf("%s\n",mini.token->str);
@@ -120,7 +119,7 @@ int main(int argc, char **argv, char **env)
 			minishell.pid = malloc(sizeof(pid_t) * (minishell.nbrofpipe = count_pipe(minishell.token)));
 			minishell.current_pipe = 0;
 			ft_executor(&minishell, minishell.env);
-			printf("$? = %d\n",minishell.exit_status);
+			printf("$? = %d\n", minishell.exit_status);
 			dup2(minishell.stdo, STDOUT);
 			dup2(minishell.stdi, STDIN);
 			free(minishell.token);
